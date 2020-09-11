fd7049c023dd3dbf29f512472b18ae62e9e87f8f
Lucas Puppim Ernesto <lucasernesto@aurearobotics.com>
2020-09-11 00:02:22 -0300

    Começar criação bot

--- inst.py
+++ inst.py
@@ -1,6 +1,22 @@
 import selenium
+from selenium import webdriver
 
-#TODO Baixar a instalar arquivo GeckoDriver
-#TODO Criar classe InstagramBot
+class InstagramBot():
+    def __init__(self):
+    #     self.username = username
+    #     self.password = password
+        self.driver = webdriver.Firefox()
 
+    def run(self):
+        self.login()
+        # https://www.instagram.com/accounts/login/?hl=pt-br&source=auth_switcher
+        # input[@name="username"]
+        # input[@name="password"]
     
+    def login(self):
+        print("aaa")
+        driver = self.driver
+        driver.get("www.google.com")
+
+
+InstagramBot().run()
